9a436fd1318fe6613fcfbef8d8a2a40118b103ab
reggol <gollog@hotmail.co.jp>
2016-07-31 12:39:06 +0900

    ライセンスヘッダ記載（デフォキャラ分） license.mdあるからやる必要なかったかも。

--- devkit100/goluah/chardll_giko/character.cpp
+++ devkit100/goluah/chardll_giko/character.cpp
@@ -1,9 +1,8 @@
-
 /*=====================================================================================
 
 	キャラクター定義
 
-	Goluah!! Copyright (C) 2001-2004 aki, 2014-2015 logger, 2004-2015 At-sushi
+	Goluah!! Copyright (C) 2001-2004 aki, 2004-2016 At-sushi, 2014-2016 logger
 
 	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
